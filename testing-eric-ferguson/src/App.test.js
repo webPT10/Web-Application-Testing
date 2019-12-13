86402180216d79bb01fa6d16b033e8dd7c3d130d
Eric Ferguson <ferguson.eric.r@gmail.com>
2019-12-13 11:11:52 -0800

    set up App.test.js -- tested Buttons and Count

--- testing-eric-ferguson/src/App.test.js
+++ testing-eric-ferguson/src/App.test.js
@@ -6,8 +6,38 @@ import "@testing-library/jest-dom/extend-expect";
 
 afterEach(rtl.cleanup)
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+it('renders without crashing!', () => {
+  const simulatedDOM = rtl.render(<App />);
+})
+
+it('All Buttons Exist', () => {
+  const simulatedDOM = rtl.render(<App />)
+
+  const ballButton = document.querySelector('div.buttons button.ball');
+  expect(ballButton).toBeInTheDocument();
+
+  const strikeButton = document.querySelector('div.buttons button.strike');
+  expect(strikeButton).toBeInTheDocument();
+
+  const foulButton = document.querySelector('div.buttons button.foul');
+  expect(foulButton).toBeInTheDocument();
+
+  const hitButton = document.querySelector('div.buttons button.hit');
+  expect(hitButton).toBeInTheDocument();
+})
+
+it('Count displayed on each Button', () => {
+  const simulatedDOM = rtl.render(<App />);
+
+  const ballCount = document.querySelector('div.counts p.ball');
+  expect(ballCount).toBeInTheDocument();
+
+  const strikeCount = document.querySelector('div.counts p.ball');
+  expect(strikeCount).toBeInTheDocument();
+
+  const foulCount = document.querySelector('div.counts p.foul');
+  expect(foulCount).toBeInTheDocument();
+
+  const hitCount = document.querySelector('div.counts p.hit');
+  expect(hitCount).toBeInTheDocument()
+})
--- testing-eric-ferguson/src/components/Dashboard.js
+++ testing-eric-ferguson/src/components/Dashboard.js
@@ -3,12 +3,12 @@ import React from 'react';
 const Dashboard = (props) => {
 
     return (
-        <>
-            <button onClick={() => props.ballChanges()}>Ball</button>
-            <button onClick={() => props.strikeChanges()}>Strike!</button>
-            <button onClick={() => props.foulChanges()}>Foul!</button>
-            <button onClick={() => props.hitChanges()}>HIT!!</button>
-        </>
+        <div className='buttons'>
+            <button className='ball' onClick={() => props.ballChanges()}>Ball</button>
+            <button className='strike' onClick={() => props.strikeChanges()}>Strike!</button>
+            <button className='foul' onClick={() => props.foulChanges()}>Foul!</button>
+            <button className='hit' onClick={() => props.hitChanges()}>HIT!!</button>
+        </div>
     )
 }
 export default Dashboard;
