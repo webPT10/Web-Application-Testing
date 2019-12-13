b360469a8a5a47f54f57107caefdd2a5fcf8b36f
Eric Ferguson <ferguson.eric.r@gmail.com>
2019-12-13 10:02:07 -0800

    App.test -- created, basic setup completed

--- testing-eric-ferguson/src/App.test.js
+++ testing-eric-ferguson/src/App.test.js
@@ -1,6 +1,10 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import App from './App';
+
+import * as rtl from '@testing-library/react';
+import "@testing-library/jest-dom/extend-expect";
+
+afterEach(rtl.cleanup)
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
