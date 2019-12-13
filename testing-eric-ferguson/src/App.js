dc9e1a9bea71fcb8e2d590bf94030778cf623809
Eric Ferguson <ferguson.eric.r@gmail.com>
2019-12-12 18:54:26 -0800

    created component folder with Display and Dashboard, both built out

--- testing-eric-ferguson/src/App.js
+++ testing-eric-ferguson/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import Dashboard from './components/Dashboard';
+import Display from './components/Display';
+
+
 function App() {
+
   return (
     <div className="App">
-      
+      <Display /> 
+      <Dashboard /> 
     </div>
   );
 }
